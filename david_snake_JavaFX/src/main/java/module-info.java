b59a53b0b7d692e16ac55c5f08742dd49a252feb
dmarino8 <98052934+dmarino8@users.noreply.github.com>
2022-05-03 18:03:12 -0500

    Add files via upload

---
+++ david_snake_JavaFX/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module marino.david_snake_javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens marino.david_snake_javafx to javafx.fxml;
+    exports marino.david_snake_javafx;
+}
